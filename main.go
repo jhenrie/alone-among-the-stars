eb6b0087a4ca368cead00a97574331c63919ccec
josephhenrie <joe.henrie@gmail.com>
2021-01-23 18:33:52 -0700

    Dice / Cards
    Built dice functionality and card functionality. Commit of working
    code and tests in prep for breaking up into modules.

--- main.go
+++ main.go
@@ -2,8 +2,44 @@ package main
 
 import (
 	"fmt"
+	"github.com/jhenrie/alone-among-the-stars/cards"
+	"github.com/jhenrie/alone-among-the-stars/dice"
 )
 
 func main() {
 	fmt.Printf("Hello World.\n")
+	d6 := dice.NewDice(6)
+	fmt.Printf("D6 Roll: %d \n", d6.Roll())
+	fmt.Printf("D6 Roll: %d \n", d6.Roll())
+	fmt.Printf("D6 Roll: %d \n", d6.Roll())
+
+	fmt.Printf("D6 Roll List: %v \n", d6.RollN(10))
+
+	d20 := dice.NewDice(20)
+	fmt.Printf("D20 Roll: %d \n", d20.Roll())
+	fmt.Printf("D20 Roll: %d \n", d20.Roll())
+	fmt.Printf("D20 Roll: %d \n", d20.Roll())
+
+	fmt.Printf("D20 Roll List: %v \n", d20.RollN(10))
+
+	deck := cards.InitDeck()
+	fmt.Printf("%s\n", deck)
+	fmt.Printf("Deck Length - %d\n", len(deck))
+
+	deck.Shuffle()
+	fmt.Printf("%s\n", deck)
+
+	card := deck.DrawOne()
+	fmt.Printf("Card %s\n", card)
+	fmt.Printf("Deck Length - %d\n", len(deck))
+
+	deck.AddCard(card)
+	fmt.Printf("Deck Length - %d\n", len(deck))
+	fmt.Printf("%s\n", deck)
+
+	card = deck.DrawOne()
+	fmt.Printf("Card %s\n", card)
+	fmt.Printf("Deck Length - %d\n", len(deck))
+
+
 }
